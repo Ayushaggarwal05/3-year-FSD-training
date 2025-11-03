511c4ff438311cf385dc9781eaa0b169d465e55b
Ayush aggarwal <aayushaggarwal348@gmail.com>
2025-11-03 13:09:38 +0530

    Updating na dmaking new book_app

---
+++ React/Book_app/src/Components/BookPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function BookPage() {
+  return (
+    <div>
+      <img src="" alt="" />
+      <h1>Title : MAth</h1>
+      <h2>Price : 250</h2>
+    </div>
+  );
+}
+
+export default BookPage;
